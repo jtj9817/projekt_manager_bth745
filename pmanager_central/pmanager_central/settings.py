ad8351f37a97edc09b3f7af436f15d7ccbc00f63
Josh J <joshuajadulco@gmail.com>
2019-10-16 01:49:41 -0400

    Added a theme for front-end

--- pmanager_central/pmanager_central/settings.py
+++ pmanager_central/pmanager_central/settings.py
@@ -55,7 +55,8 @@ ROOT_URLCONF = 'pmanager_central.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- pmanager_central/project_manager/views.py
+++ pmanager_central/project_manager/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+	return render(request, 'index.html')
+
+def login_user(request):
+	if request.method == "POST":
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			if user.is_active:
+				login(request, user)
+				return render(request, 'index.html')
+			else:
+				return render(request, 'login.html', {'error_message': 'Account is disabled.'})
+		else:
+			return render(request, 'login.html', {'error_message': 'Invalid login'})
+	return render(request, 'login.html')	
+
+#User submits the form filled with data in a POST request
+def register(request):
+	if request.method == 'POST':
+		form = UserForm(request.POST)
+		if form.is_valid():
+			user= form.save(commit=False)
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user.set_password(password)
+			user.save()
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			return render(request,'registersuccess.html')
+	else:
+		form = UserForm()
+	return render(request, 'registration_form.html', {'form': form})
