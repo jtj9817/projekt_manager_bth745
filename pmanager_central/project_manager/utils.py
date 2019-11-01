a2f3ed0c6c65e13fe2ce1849d6235c391a2c0f94
MahnoorZS <36709422+MahnoorZS@users.noreply.github.com>
2019-11-01 17:57:58 -0400

    Delete utils.py

--- pmanager_central/project_manager/utils.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth import get_user
-
-class ProfileGetObjectMixin:
-	def get_object(self, queryset=None):
-		current_user = get_user(self.request)
-		return current_user.profile
